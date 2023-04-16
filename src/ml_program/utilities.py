32eeafde558563605f45e9160392c8f1176bb19e
JA Simmons V <simmonsj@redkeep.com>
2023-04-16 15:32:25 -0500

    Build utilities.py
    Add various common functions to utilities.py

---
+++ src/ml_program/utilities.py
@@ -0,0 +1,201 @@
+# -*- coding: utf-8 -*-
+"""shared utilities for supervised learning algorithm for classification and regression.
+
+Full license in LICENSE.md
+"""
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+
+# Global dict to convert various headers into a central, common header structure.
+CONVERT_HEADERS = {"Dst Port": ["Destination Port"],
+                   "Protocol": ["Protocol"],
+                   "Flow Duration": ["Flow Duration"],
+                   "Tot Fwd Pkts": ["Total Fwd Packets"],
+                   "Tot Bwd Pkts": ["Total Backward Packets"],
+                   "TotLen Fwd Pkts": ["Total Length of Fwd Packets"],
+                   "TotLen Bwd Pkts": ["Total Length of Bwd Packets"],
+                   "Flow Byts/s": ["Flow Bytes/s"],
+                   "Flow Pkts/s": ["Flow Packets/s"],
+                   "Fwd IAT Tot": ["Fwd IAT Total"],
+                   "Bwd IAT Tot": ["Bwd IAT Total"],
+                   "Fwd PSH Flags": ["Fwd PSH Flags"],
+                   "Bwd PSH Flags": ["Bwd PSH Flags"],
+                   "Fwd URG Flags": ["Fwd URG Flags"],
+                   "Bwd URG Flags": ["Bwd URG Flags"],
+                   "Fwd Header Len": ["Fwd Header Length"],
+                   "Bwd Header Len": ["Bwd Header Length"],
+                   "Fwd Pkts/s": ["Fwd Packets/s"],
+                   "Bwd Pkts/s": ["Bwd Packets/s"],
+                   "FIN Flag Cnt": ["FIN Flag Count"],
+                   "SYN Flag Cnt": ["SYN Flag Count"],
+                   "RST Flag Cnt": ["RST Flag Count"],
+                   "PSH Flag Cnt": ["PSH Flag Count"],
+                   "ACK Flag Cnt": ["ACK Flag Count"],
+                   "URG Flag Cnt": ["URG Flag Count"],
+                   "CWE Flag Count": ["CWE Flag Count"],
+                   "ECE Flag Cnt": ["ECE Flag Count"],
+                   "Down/Up Ratio": ["Down/Up Ratio"],
+                   "Subflow Fwd Pkts": ["Subflow Fwd Packets"],
+                   "Subflow Fwd Byts": ["Subflow Fwd Bytes"],
+                   "Subflow Bwd Pkts": ["Subflow Bwd Packets"],
+                   "Subflow Bwd Byts": ["Subflow Bwd Bytes"],
+                   "Init Fwd Win Byts": ["Init_Win_bytes_forward"],
+                   "Init Bwd Win Byts": ["Init_Win_bytes_backward"],
+                   "Fwd Act Data Pkts": ["act_data_pkt_fwd"],
+                   "Fwd Seg Size Min": ["min_seg_size_forward"],
+                   "Label": ["Label"]
+                   }
+
+# The list of features for detecting bruteforce (FTP/SSH) attacks.
+BRUTEFORCE_FEATURES = ['Dst Port',
+                       'Protocol',
+                       'Flow Duration',
+                       'Tot Fwd Pkts',
+                       'Tot Bwd Pkts',
+                       'TotLen Fwd Pkts',
+                       'TotLen Bwd Pkts',
+                       'Flow Byts/s',
+                       'Flow Pkts/s',
+                       'Fwd IAT Tot',
+                       'Bwd IAT Tot',
+                       'Fwd PSH Flags',
+                       'Bwd PSH Flags',
+                       'Fwd URG Flags',
+                       'Bwd URG Flags',
+                       'Fwd Header Len',
+                       'Bwd Header Len',
+                       'Fwd Pkts/s',
+                       'Bwd Pkts/s',
+                       'FIN Flag Cnt',
+                       'SYN Flag Cnt',
+                       'RST Flag Cnt',
+                       'PSH Flag Cnt',
+                       'ACK Flag Cnt',
+                       'URG Flag Cnt',
+                       'CWE Flag Count',
+                       'ECE Flag Cnt',
+                       'Down/Up Ratio',
+                       'Subflow Fwd Pkts',
+                       'Subflow Fwd Byts',
+                       'Subflow Bwd Pkts',
+                       'Subflow Bwd Byts',
+                       'Init Fwd Win Byts',
+                       'Init Bwd Win Byts',
+                       'Fwd Act Data Pkts',
+                       'Fwd Seg Size Min',
+                       ]
+
+
+def convert(lst):
+    """convert a list into a dict of very specific shape
+
+    This method will take a simple list, and convert that list into a dict that is value: place
+    e.g., ['one', 'two', 'three'] returns {'one': 0, 'two': 1, 'three': 2}
+    This is required for DataFrame mapping of values from string to an integer in order of appearance.
+
+    :param lst: list to convert into a dict
+    :return: (dict)
+    """
+    res_dct = {lst[i]: i for i in range(0, len(lst))}
+    return res_dct
+
+
+def fix_headers(data):
+    """Normalize headers from packet dumps.
+
+    Normalize headers using the CONVERT_HEADERS dict to account for documented possible changes in header table.
+    This will also strip all white space at the beginning and end of the header string.
+
+    :param data: Raw Dataframe
+    :return:
+    """
+    old_lst = []
+    fix_lst = []
+    # Strip beginning and end white spaces in headers
+    for column in data.columns:
+        old_lst.append(column)
+        fix_lst.append(column.strip())
+    res_dct = {old_lst[i]: fix_lst[i] for i in range(len(data.columns))}  # Build converting dict for DataFrame.Rename
+    data = data.rename(res_dct, axis='columns')
+
+    # Convert any headers according to the CONVERT_HEADERS dict.
+    old_lst = []
+    fix_lst = []
+    red_dct = None
+    for column in data.columns:
+        for header in CONVERT_HEADERS:
+            if column in CONVERT_HEADERS[header]:
+                old_lst.append(column)
+                fix_lst.append(header)
+    res_dct = {old_lst[i]: fix_lst[i] for i in range(len(old_lst))}  # Build converting dict for DataFrame.Rename
+    data = data.rename(res_dct, axis='columns')
+    return data
+
+
+def get_target_names(data, column='Label'):
+    """return a list of unique values
+
+    This method was built to get a list of target labels for supervised learning. The dataframe will return the target
+    values in the order they first appear and build an
+
+    :param data:
+    :return:
+    """
+    return np.unique(data[column].values)
+
+
+def get_targets_int(data, column='Label'):
+    """Get the list of target values from the Label column
+
+    Grabs just the supervised training results, and maps it as an ordered integer according to the "get_target_names"
+    method.
+
+    :return: (list) values from `Label` column mapped as integer values.
+    """
+    target_names = get_target_names(data)
+    return data[column].map(convert(target_names))
+
+
+def data_training_prep(data, targets):
+    """Prepare data for training
+
+    :param data: (pandas.DataFrame) Data set without the answer column.
+    :param targets: (pandas.Series) A pandas.Series (a single column) of the supervised data results to measure against.
+    :return: (pandas.DataFrame) and (pandas.Series) of cleaned data removing any invalid data points.
+    """
+    # Build target values for decison tree classifier
+    # Iterate over the columns in the dataframe to check if they are strings
+    le = LabelEncoder()
+    for col in data.columns:
+        if data[col].dtypes not in ['int64', 'float64']:
+            data[col] = le.fit_transform(data[col])
+
+    # Search for the columns with infinite values
+    lt_columns = data[data.columns[data.max() == np.inf]].columns
+
+    # modify infinite values (10 x max)
+    for st_column_inf in lt_columns:
+        df_column_aux = data[st_column_inf]
+        # identify the max value
+        vl_max_aux = df_column_aux[df_column_aux < np.inf].max()
+        # .loc is important to modify the value in the dataframe
+        data.loc[data[st_column_inf] == np.inf, st_column_inf] = 10*vl_max_aux
+
+    # check if there are still columns with infinite values
+    lt_columns = data[data.columns[data.max() == np.inf]].columns
+    assert len(lt_columns) == 0
+
+    # Search for the columns with NaN values
+    # for st_column_nan in data.columns:
+    #     df_column_aux = data[data[st_column_nan].isna()].copy()
+    #     if len(df_column_aux) > 0:
+    #         print('df_column_aux:\n{}'.format(df_column_aux.transpose()))
+    #         print('Transpose:\n{}\n'.format(df_target[df_features[st_column_nan].isna()].transpose()))
+    #         print('st_column_nan: {}'.format(st_column_nan))
+    #         print('The total amount of NaNs are: {}'.format(len(data[data[st_column_nan].isna()])))
+    #         print('*** data ***\n{}'.format(data[st_column_nan].describe()))
+    # Drop the rows with NaN values
+    data.dropna(inplace=True)
+    targets = targets[targets.index.isin(data.index)]
+    assert len(data) == len(targets)
+    return data, targets
